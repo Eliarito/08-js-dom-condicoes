dd6b9f99c40483f7abd6348ccba512880c17368c
Eliarito <133966592+Eliarito@users.noreply.github.com>
2023-07-06 11:58:20 +0000

    Atualização 2.0

--- script.js
+++ script.js
@@ -6,12 +6,12 @@ function carregar() {
     msg.innerHTML = `Agora são ${hora} horas.`
     if (hora >= 0 && hora < 12){
         //Bom Dia!
-        img.src = 'fotomanha.png'
+        img.src = 'fotomanha.jpg'
     }else if (hora >= 12 && hora < 18){
         //Boa Tarde!
-        img.src = 'fototarde.png'
+        img.src = 'fototarde.jpg'
     }else{
         //Boa Noite!
-        img.src = 'fotonoite.png'
+        img.src = 'fotonoite.jpg'
     }
 }
